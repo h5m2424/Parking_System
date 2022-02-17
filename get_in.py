40f384e91bd38f74c66709d03a99d52a19f52ebf
h5m2424 <h5m2424@qq.com>
2022-02-17 22:45:24 +0800

    update get_out: payment calculate

--- get_in.py
+++ get_in.py
@@ -4,9 +4,6 @@
 3.如果车辆未在场内，判断是否有剩余车位，如果没有剩余车位闸门不开启；并显示“车位已满！”
 4.如果还有剩余车位，闸门开启；并显示“剩余车位”，“每小时收费价格”，”车牌号“，“入场时间”；剩余车位-1
 5.车辆车牌和入场时间放入字典
-6.车辆出场，扫描到车牌
-7.显示“停车费”，“停车时长”，“入场时间”，“出场时间”
-8.确认收费后闸门开启，剩余车位+1
 """
 import time
 import ast
@@ -15,7 +12,7 @@ import os.path
 
 def open_gate(car_num):
     # do some action
-    # to open the date
+    # to open the gate
     print("欢迎光临！", car_num)
 
 
@@ -31,17 +28,16 @@ def writing_file_ap(available_place):
 
 def in_parking(parking_zone, available_place, parking_price, car_num):
     if car_num in parking_zone:
-        print("车辆已经在场内！")
+        print("车辆已经在场内，请联系门卫人员！")
     else:
         if available_place == 0:
             print("车位已满！")
         else:
             open_gate(car_num)
             in_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            print("剩余车位：", str(available_place) + "个")
-            print("每小时收费：", str(parking_price) + "元")
+            print("剩余车位：", available_place, "个")
+            print("每小时收费：", parking_price, "元")
             print("入场时间：", in_time)
-            print("剩余车位：", available_place)
             available_place -= 1
             parking_zone[car_num] = in_time
             writing_file_pz(parking_zone)
@@ -52,15 +48,17 @@ if __name__ == "__main__":
     if os.path.exists("parking_zone.txt"):
         parking_zone_io = open("parking_zone.txt", "r", encoding="utf-8")
         parking_zone_str = parking_zone_io.readline()
-        parking_zone = ast.literal_eval(parking_zone_str)
+        parking_zone = ast.literal_eval(parking_zone_str) #
     else:
         parking_zone = {}
+    
     if os.path.exists("available_place.txt"):
         available_place_io = open("available_place.txt", "r", encoding="utf-8")
         available_place_str = available_place_io.readline()
         available_place = int(available_place_str)
     else:
-        available_place = 5
-    parking_price = 10
+        available_place = 50 # 车位总数
+    
+    parking_price = 10 # 每小时停车费
     car_num = input("车牌已被扫描：")
     in_parking(parking_zone, available_place, parking_price, car_num)
---
+++ get_out.py
@@ -0,0 +1,76 @@
+"""
+1.车辆出场，扫描到车牌
+2.判断车辆在不在字典里，如果不在提示“没有车辆入场纪录，请咨询保安！”。
+3.如果车辆在字典里，显示“停车费”，“停车时长”，“入场时间”，“出场时间”
+4.确认收费后闸门开启，从字典里删除车辆，剩余车位+1
+"""
+from datetime import datetime
+import ast
+import os.path
+
+
+def open_gate(car_num):
+    # do some action
+    # to open the gate
+    print("一路顺风！", car_num)
+
+
+def writing_file_pz(parking_zone):
+    with open("parking_zone.txt", "w", encoding="utf-8") as fo:
+        fo.write(str(parking_zone))
+
+
+def writing_file_ap(available_place):
+    with open("available_place.txt", "w", encoding="utf-8") as fo:
+        fo.write(str(available_place))
+
+
+def out_parking(parking_zone, available_place, parking_price, car_num):
+    if car_num not in parking_zone:
+        print("未查询到入场纪录，请联系门卫人员！")
+    
+    else:
+        out_time = datetime.today()
+        parking_time_in_second = (out_time - datetime.strptime(parking_zone[car_num], "%Y-%m-%d %H:%M:%S")).seconds
+
+        if parking_time_in_second / 3600 < 1: # 1小时免收停车费
+            print("停车时长：", parking_time_in_second / 3600, "小时")
+            open_gate(car_num)
+            available_place += 1
+            parking_zone.pop(car_num)
+            writing_file_pz(parking_zone)
+            writing_file_ap(available_place)
+
+        else:    
+            print("每小时收费：", parking_price, "元")
+            print("停车时长：", parking_time_in_second / 3600, "小时")
+            print("停车费用：", parking_time_in_second / 3600 * parking_price, "元")
+            pay = int(input("请交费:"))
+            while pay < parking_time_in_second / 3600 * parking_price:
+                pay = int(input("请交费:"))
+            else:
+                open_gate(car_num)
+                available_place += 1
+                parking_zone.pop(car_num)
+                writing_file_pz(parking_zone)
+                writing_file_ap(available_place)
+
+
+if __name__ == "__main__":
+    if os.path.exists("parking_zone.txt"):
+        parking_zone_io = open("parking_zone.txt", "r", encoding="utf-8")
+        parking_zone_str = parking_zone_io.readline()
+        parking_zone = ast.literal_eval(parking_zone_str) #
+    else:
+        parking_zone = {}
+    
+    if os.path.exists("available_place.txt"):
+        available_place_io = open("available_place.txt", "r", encoding="utf-8")
+        available_place_str = available_place_io.readline()
+        available_place = int(available_place_str)
+    else:
+        available_place = 50 # 车位总数
+    
+    parking_price = 10 # 每小时停车费
+    car_num = input("车牌已被扫描：")
+    out_parking(parking_zone, available_place, parking_price, car_num)
